59828141523ea9bd21e68b765a75f45340fd2d01
Joao Ferreira <joao.ferreira@equals.com.br>
2023-09-23 22:16:13 -0300

    view criado, e footer componetizado

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="http://aimvx.com.br">
+                        Samily App
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Ajuda
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Licença de Uso
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright ml-auto">
+            2023, criado <i class="la la-heart heart text-danger"></i> por <a
+                href="http://aimvx.com.br">Samily App</a>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/create-tipo-veiculo', 'App\Http\Controllers\TipoVeiculoController@create');
